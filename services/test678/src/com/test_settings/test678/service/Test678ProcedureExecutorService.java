88e4e248b7b4248d064139fc5748e6217ba8eb7f
Surya Santosh Mani T null <surya.talluri@wavemaker.com>
2015-11-26 10:01:37 +0000

    New files added - [test678_vk.hbm.xml,test_procedure_query.json,T4375ProcedureExecutorServiceImpl.java,BookServiceImpl.java,ui-icons_777777_256x240.png,t4375_published_dataModel.json,ProcedureExecutionController.java,Test_procedureQueryExecutorServiceImpl.java,LIBRARYProcedureExecutorServiceImpl.java,FontAwesome.otf,LIBRARY_published_dataModel.json,Test_procedureQueryExecutorService.java,Test678QueryExecutorService.java,Test678ProcedureExecutorServiceImpl.java,db-connection-settings.json,ui-icons_777620_256x240.png,test_procedure_API.json,ui-icons.png,spinner-small.gif,QueryExecutionController.java,test_procedure-procedures.mappings.json,LIBRARYProcedureExecutorService.java,test678-queries.hbm.xml,t4375_vk.hbm.xml,wm-mobileloader.min.js,ui-bg_glass_65_ffffff_1x400.png,glyphicons-halflings-regular.woff2,wm-style.css,ui-icons_444444_256x240.png,LIBRARY_procedure.json,Test_procedureProcedureExecutorServiceImpl.java,ui-icons_888888_256x240.png,t4375.properties,t4375_procedure.json,ui-bg_flat_75_ffffff_40x100.png,ProcedureExecutionController.java,servicedef.xml,BookController.java,test678.hbm.xml,LIBRARY_API.json,glyphicons-halflings-regular.svg,Table1ServiceImpl.java,bootstrap.css.map,EmployeeController.java,db-connection-settings.json,test678-procedures.mappings.json,ui-icons_cc0000_256x240.png,ui-bg_flat_0_aaaaaa_40x100.png,t4375.spring.xml,test_procedure.spring.xml,servicedef.xml,Test_procedureProcedureExecutorService.java,servicedef.xml,Table2Service.java,db-connection-settings.json,test678_API.json,Test678ProcedureExecutorService.java,Table1.java,ui-icons_ffffff_256x240.png,EmployeeDao.java,Table1Controller.java,test678_procedure.json,ui-icons_454545_256x240.png,Table2ServiceImpl.java,ui-bg_glass_95_fef1ec_1x400.png,test_procedure_published_dataModel.json,Test678QueryExecutorServiceImpl.java,Table1Id.java,Table2.java,Table2Controller.java,BookService.java,Employee.java,fontawesome-webfont.woff,T4375QueryExecutorService.java,ui-bg_glass_75_e6e6e6_1x400.png,glyphicons-halflings-regular.woff,t4375_query.json,fontawesome-webfont.svg,ui-icons_2e83ff_256x240.png,ui-bg_highlight-soft_75_cccccc_1x100.png,test678_query.json,test678.properties,servicedef.xml,test_procedure_procedure.json,T4375ProcedureExecutorService.java,LIBRARYQueryExecutorServiceImpl.java,ProcedureExecutionController.java,LIBRARY.properties,glyphicons-halflings-regular.ttf,QueryExecutionController.java,glyphicons-halflings-regular.eot,fontawesome-webfont.eot,Table1Dao.java,test_procedure.properties,EmployeeServiceImpl.java,test678.spring.xml,T4375QueryExecutorServiceImpl.java,test_procedure.hbm.xml,LIBRARY-procedures.mappings.json,Table2Id.java,wm-libs.min.js,LIBRARY-queries.hbm.xml,t4375-queries.hbm.xml,LIBRARY.spring.xml,LIBRARYQueryExecutorService.java,ui-icons_555555_256x240.png,ui-icons_222222_256x240.png,ProcedureExecutionController.java,test_procedure-queries.hbm.xml,EmployeeService.java,QueryExecutionController.java,BookDao.java,LIBRARY.hbm.xml,LIBRARY_vk.hbm.xml,LIBRARY_query.json,Table1Service.java,ui-icons_cd0a0a_256x240.png,QueryExecutionController.java,test678_published_dataModel.json,t4375_API.json,db-connection-settings.json,t4375-procedures.mappings.json,ui-bg_glass_55_fbf9ee_1x400.png,fontawesome-webfont.ttf,Table2Dao.java,test_procedure_vk.hbm.xml,fontawesome-webfont.woff2,loader.gif,Book.java,ui-bg_glass_75_dadada_1x400.png] Files modified - [Main.variables.json,Main.html,production.properties,login.html,project-services.xml,pom.xml,index.html,.wmproject.properties,page.min.html,wmProperties.js,app.variables.json,development.properties] Refactored files - [] Fixed bugs - [] Removed files - []

---
+++ services/test678/src/com/test_settings/test678/service/Test678ProcedureExecutorService.java
@@ -0,0 +1,22 @@
+/*Copyright (c) 2015-2016 wavemaker.com All Rights Reserved.
+ This software is the confidential and proprietary information of wavemaker.com You shall not disclose such Confidential Information and shall use it only in accordance
+ with the terms of the source code license agreement you entered into with wavemaker.com*/
+
+
+package com.test_settings.test678.service;
+
+import java.util.Map;
+import java.util.List;
+
+
+import com.wavemaker.runtime.data.model.CustomProcedure;
+import com.wavemaker.runtime.data.exception.QueryParameterMismatchException;
+
+public interface Test678ProcedureExecutorService {
+
+	
+	List<Object> executeWMCustomProcedure(CustomProcedure procedure) ;
+
+
+}
+
